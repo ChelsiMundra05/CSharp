e43056af46a05fdcfd8e8b5caadc4a65a69c18ef
TARUN <chelsimaheswari17@gmail.com>
2021-02-25 01:41:38 +0530

    Assignment-4 Submitted.

---
+++ Assignment4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Assignment4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await ReadAndWriteFileAsync();
+        }
+
+        private static async Task ReadAndWriteFileAsync()
+        {
+            string ReadFilePath ="A.txt";
+            string content = await File.ReadAllTextAsync(ReadFilePath);
+            if(string.IsNullOrEmpty(content))
+            {
+                throw new ArgumentNullException("'A.txt 'File is Empty.");
+            }
+            else
+            {
+                string WriteFilePath ="B.txt";
+                await File.WriteAllTextAsync(WriteFilePath,content); 
+                Console.WriteLine("Successfully Content of 'A.txt' File copied to 'B.txt' File.");  
+            }
+        }
+    }
+}
